310a43d0427079a2f42f5ac87ed77c4022398d29
thanneeruravi22 <thanneeruravi22@gmail.com>
2020-08-19 15:26:24 +0000

    Changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>. as
+          Edit <code>src/App.js</code>. Ravi Thanneeru
         </p>
         <a
           className="App-link"
